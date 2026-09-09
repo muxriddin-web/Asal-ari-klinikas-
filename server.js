850b843c83a26bb531b12ef4241af50b523986f3
Muxriddin O'tkirov <otkirovmuxriddin01@gmail.com>
2026-09-09 10:41:56 +0500

    Refactor server.js for improved clarity

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 
-// Xosting taqdim etadigan portni olish yoki bo'lmasa standart 8080
+// Xosting taqdim etadigan portni olish yoki bo'lmasa standart 5000
 const PORT = process.env.PORT || 5000;
 
 // Frontend fayllarni ulashi
