fa67f7202cae9f3ba9007b6e875847e9e48fed3f
Muxriddin O'tkirov <otkirovmuxriddin01@gmail.com>
2026-09-09 10:40:08 +0500

    Change default server port to 5000
    Updated the default port from 3000 to 5000.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 
-// Xosting taqdim etadigan portni olish yoki bo'lmasa standart 3000
+// Xosting taqdim etadigan portni olish yoki bo'lmasa standart 5000
 const PORT = process.env.PORT || 8080;
 
 // Frontend fayllarni ulashi
