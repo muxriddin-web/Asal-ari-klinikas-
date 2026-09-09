8b6ceac2fe2ad40a8f0dd4350905101b40e703ae
Muxriddin O'tkirov <otkirovmuxriddin01@gmail.com>
2026-09-09 10:39:20 +0500

    Change server port from 5000 to 8080

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 
 // Xosting taqdim etadigan portni olish yoki bo'lmasa standart 3000
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Frontend fayllarni ulashi
 app.use(express.static(path.join(__dirname, 'public')));
