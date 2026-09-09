1b21b67e210f49d0277e626d5bd87b0eb527c0ee
Muxriddin O'tkirov <otkirovmuxriddin01@gmail.com>
2026-09-09 10:42:29 +0500

    Change server port from 5000 to 3000

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 
 // Xosting taqdim etadigan portni olish yoki bo'lmasa standart 5000
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Frontend fayllarni ulashi
 app.use(express.static(path.join(__dirname, 'public')));
