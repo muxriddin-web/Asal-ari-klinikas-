06be7ce7672adb632babfbb8034407fef439bddc
Muxriddin O'tkirov <otkirovmuxriddin01@gmail.com>
2026-09-10 10:43:07 +0500

    Refactor theme toggle and contact form logic

--- public/main.js
+++ public/main.js
@@ -1,4 +1,4 @@
-// DARK/Light toggle funksiyasi (O'zgartirilmagan qism uchun)/////////////////
+// DARK/LIGHT toggle funksiyasi (O'zgartirilmagan qism uchun)/////////////////
 const themeBtn = document.getElementById('theme-btn');
 
 function applyTheme(theme) {
